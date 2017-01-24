2d47a110dd13626c0a3b019bd78be09eddd1be83
Edward <ed@flynowpaylater.com>
2017-01-24 12:46:49 +0700

    added query log to native methods

--- src/Native.php
+++ src/Native.php
@@ -11,51 +11,81 @@
 
 		// http://php.net/manual/en/pdo.begintransaction.php
 		public function beginTransaction(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[beginTransaction]', '');
+			}
 			return $this->createPDO()->beginTransaction();
 		}
 
 		// http://php.net/manual/en/pdo.commit.php
 		public function commit(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[commit]', '');
+			}
 			return $this->createPDO()->commit();
 		}
 
 		// http://php.net/manual/en/pdo.errorcode.php
 		public function errorCode(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[errorCode]', '');
+			}
 			return $this->createPDO()->errorCode();
 		}
 
 		// http://php.net/manual/en/pdo.errorinfo.php
 		public function errorInfo(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[errorInfo]', '');
+			}
 			return $this->createPDO()->errorInfo();
 		}
 
 		// http://php.net/manual/en/pdo.exec.php
 		public function exec($statement){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[errorInfo]', $statement);
+			}
 			return $this->createPDO()->exec($statement);
 		}
 
 		// http://php.net/manual/en/pdo.getattribute.php
 		public function getAttribute($attribute){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[getAttribute]', $name);
+			}
 			return $this->createPDO()->getAttribute($name);
 		}
 
 		// http://php.net/manual/en/pdo.getavailabledrivers.php
 		public function getAvailableDrivers(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[getAvailableDrivers]', '');
+			}
 			return $this->createPOD()->getAvailableDrivers();
 		}
 
 		// http://php.net/manual/en/pdo.intransaction.php
 		public function inTransaction(){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[inTransaction]', '');
+			}
 			return $this->createPDO()->inTransaction();
 		}
 
 		// http://php.net/manual/en/pdo.lastinsertid.php
 		public function lastInsertId($name = null){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[lastInsertId]', $name);
+			}
 			return $this->createPDO()->lastInsertId($name);
 		}
 
 		// http://php.net/manual/en/pdo.prepare.php
 		public function prepare($statement, $driver_options = []){
+			if($this->isLogEnabled()){
+				$this->logQuery('native-pdo[prepare]', [$statement, $driver_options]);
+			}
 			return $this->createPDO()->prepare($statement, $driver_options);
 		}
 
@@ -76,6 +106,8 @@
 				$options[] = $info2;
 			}
 
+			$this->logQuery('native-pdo[query]', $options);
+
 			return call_user_func_array([$pdo, 'query'], $options);
 		}
 
@@ -88,16 +120,20 @@
 				$options[] = $parameter_type;
 			}
 
+			$this->logQuery('native-pdo[quote]', $options);
+
 			return call_user_func_array([$pdo, 'quote'], $options);
 		}
 
 		// http://php.net/manual/en/pdo.rollback.php
 		public function rollBack(){
+			$this->logQuery('native-pdo[rollBack]', '');
 			return $this->createPDO()->rollBack();
 		}
 
 		// http://php.net/manual/en/pdo.setattribute.php
 		public function setAttribute($attribute, $value){
+			$this->logQuery('native-pdo[setAttribute]', [$attribute,$value]);
 			return $this->createPDO()->setAttribute($attribute, $value);
 		}
 
